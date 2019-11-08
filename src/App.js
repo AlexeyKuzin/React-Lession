4715f52e14bff2aeca1d1b3015889644d6b93dfb
Alexey Kuzin <aleksey.kuzin@mail.ru>
2019-11-08 16:12:43 +0400

    commit-2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div style={{color: 'blue', fontSize: '25px', border: 'solid 2px red'}}>
       <h1>Hello world!</h1>
     </div>
   );
